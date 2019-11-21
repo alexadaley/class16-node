eb914009d3af99617e56994bfa28c4f669c6fdff
alexadaley <a96466@aup.edu>
2019-11-21 15:42:57 +0000

    pushing so i dont lose mongo

---
+++ models/models.class19.js
@@ -0,0 +1,13 @@
+exports.getProducts = async function() {
+var MongoClient = require('mongodb').MongoClient
+let client = await MongoClient.connect(
+'mongodb://0.0.0.0:27017',
+{useUnifiedTopology: true}
+)
+let db = client.db('cs3051')
+let result = await db.collection('users').
+find().toArray()
+client.close();
+console.log(result)
+return result
+}
